ac7f2450bf92a9f76014e4e1bb7e32ce995d1a92
Graham1228 <grotieno12@gmail.com>
2020-05-06 07:02:09 +0300

    fourth commit

--- js/script.js
+++ js/script.js
@@ -2,5 +2,9 @@ var gender=document.getElementById('gender');
   if(gender=="" || gender==null){
     alert("Please enter a valid gender!");
   }
-  var d = new Date();
+  var daysOfTheWeek=["Sunday", "Monday", "Tuesday", "Thursday", "Friday", "Saturday"]
+  var d = Date(((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7);
+  d=(31>d>=0)
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var daysOfTheWeek=["Sunday", "Monday", "Tuesday", "Thursday", "Friday", "Saturday"]
