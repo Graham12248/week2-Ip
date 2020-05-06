f555afda9bdebc398c0ad899484e7c248e1fc226
Graham1228 <grotieno12@gmail.com>
2020-05-06 05:52:53 +0300

    added months on script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,5 @@ var gender=document.getElementById('gender');
   if(gender=="" || gender==null){
     alert("Please enter a valid gender!");
   }
+  var d = new Date();
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
